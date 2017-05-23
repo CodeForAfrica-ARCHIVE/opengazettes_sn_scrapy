afc8ac265c209477aba6054ec582bdbb2e30f4df
GathuBoswell <gathudev@gmail.com>
2017-05-23 06:36:17 +0300

    Refactor for python2  * remove python3 syntax  * remove debugging print statements

--- opengazettes_sn/pipelines.py
+++ opengazettes_sn/pipelines.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import os.path
 import logging
 from scrapy.http import Request
 from scrapy.utils.request import referer_str
@@ -14,52 +13,12 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
-from collections import defaultdict
-
-class FSFilesStore(object):
-
-    def __init__(self, basedir):
-        if '://' in basedir:
-            basedir = basedir.split('://', 1)[1]
-        self.basedir = basedir
-        self._mkdir(self.basedir)
-        self.created_directories = defaultdict(set)
-
-    def persist_file(self, path, buf, info, meta=None, headers=None):
-        absolute_path = self._get_filesystem_path(path)
-        self._mkdir(os.path.dirname(absolute_path), info)
-        with open(absolute_path, 'rb') as f:
-            f.write(buf.getvalue())
-        print('I have reache write buaaaaaaaaaaaaaananananananananananananananananananananananananananananananaan')
-    def stat_file(self, path, info):
-        absolute_path = self._get_filesystem_path(path)
-        try:
-            last_modified = os.path.getmtime(absolute_path)
-        except os.error:
-            return {}
-
-        with open(absolute_path, 'rb') as f:
-            checksum = md5sum(f)
-
-        return {'last_modified': last_modified, 'checksum': checksum}
-
-    def _get_filesystem_path(self, path):
-        path_comps = path.split('/')
-        return os.path.join(self.basedir, *path_comps)
-
-    def _mkdir(self, dirname, domain=None):
-        seen = self.created_directories[domain] if domain else set()
-        if dirname not in seen:
-            if not os.path.exists(dirname):
-                os.makedirs(dirname)
-            seen.add(dirname)
 
 class OpengazettesSnFilesPipeline(FilesPipeline):
 
     loop = []
 
     def media_downloaded(self, response, request, info):
-        print('yeeh i have been called, woohoo: ', response.url)
         referer = referer_str(request)
 
         if response.status != 200:
@@ -82,7 +41,6 @@ class OpengazettesSnFilesPipeline(FilesPipeline):
             raise FileException('empty-content')
 
         status = 'cached' if 'cached' in response.flags else 'downloaded'
-        print('status is as follows', status )
         logger.debug(
             'File (%(status)s): Downloaded file from %(request)s referred in '
             '<%(referer)s>',
@@ -92,9 +50,7 @@ class OpengazettesSnFilesPipeline(FilesPipeline):
         self.inc_stats(info.spider, status)
 
         try:
-            print('Ihave reached try')
             path = self.file_path(request, response=response, info=info)
-            print('this the path ', path)
             checksum = self.file_downloaded(response, request, info)
         except FileException as exc:
             logger.warning(
@@ -116,7 +72,6 @@ class OpengazettesSnFilesPipeline(FilesPipeline):
         return {'url': request.url, 'path': path, 'checksum': checksum }
 
     def get_media_requests(self, item, info):
-        print('this is the item for filename: ', item['filename'])
         return [Request(x, meta={'filename': item["filename"],
                 'gazette_year': item['gazette_year'], 'gazette_month': item['gazette_month'],
                                  'file_urls_len': len(item['file_urls'])})
@@ -159,26 +114,22 @@ class OpengazettesSnFilesPipeline(FilesPipeline):
         if len(self.loop) == request.meta['file_urls_len']:
             cont = ''
             for item in self.loop:
-                cont += item.decode("utf-8") + '\n'
+                cont += item + '\n'
 
-            buf = BytesIO(bytes(cont, 'utf-8'))
+            buf = BytesIO(cont.encode('ascii', 'ignore'))
             checksum = md5sum(buf)
             buf.seek(0)
             self.store.persist_file(path, buf, info)
             self.loop = []
-            print('file saved: ', path)
             return checksum
-        print('length of files is ========> ', len(self.loop))
-        print('length of file_urls is ========> ', request.meta['file_urls_len'])
         return None
 
     def modify_response(self, response):
-        print('type of response body', type(response.body))
         article_contents = response.xpath('//*[@id="explorei"]/div[2]').extract()
         content = ''
         for article_content in article_contents:
             content += article_content + '\n'
 
-        return bytes(content, 'utf-8')
+        return content
 
 
