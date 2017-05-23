93d94448b875446c86596b71382dd5cb52e79ff9
GathuBoswell <gathudev@gmail.com>
2017-05-23 13:11:41 +0300

    Remove inline comments

--- opengazettes_sn/items.py
+++ opengazettes_sn/items.py
@@ -10,14 +10,14 @@ import scrapy
 
 class OpengazettesSnItem(scrapy.Item):
     # define the fields for your item here like:
-    gazette_link = scrapy.Field() # link to the gazette on http://www.jo.gouv.sn/spip.php
-    publication_date = scrapy.Field() # date the gazette is published
+    gazette_link = scrapy.Field()
+    publication_date = scrapy.Field()
     gazette_number = scrapy.Field()
     files = scrapy.Field()
-    file_urls = scrapy.Field() # path to the file
-    gazette_links = scrapy.Field() # I am not sure what this is
-    filename = scrapy.Field() # the new filename
-    gazette_title = scrapy.Field() # title of the gazette
+    file_urls = scrapy.Field()
+    gazette_links = scrapy.Field()
+    filename = scrapy.Field()
+    gazette_title = scrapy.Field()
     gazette_day = scrapy.Field()
     gazette_month = scrapy.Field()
     gazette_year = scrapy.Field()
